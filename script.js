bb98ce6ff91fb0a39938f2e1fcf522ec6d0ff858
Reach Rimon <reachrimon@gmail.com>
2023-01-24 16:52:19 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let plus = document.querySelector(".plus");
+let minus = document.querySelector(".minus");
+let multiply = document.querySelector(".multiply");
+let divide = document.querySelector(".divide");
+
+let a = document.querySelector(".a");
+let b = document.querySelector(".b");
+let c = document.querySelector(".c");
+let d = document.querySelector(".d");
+let e = document.querySelector(".e");
+let f = document.querySelector(".f");
+let g = document.querySelector(".g");
+let h = document.querySelector(".h");
+let i = document.querySelector(".i");
+let j = document.querySelector(".j");
+let k = document.querySelector(".k");
+let l = document.querySelector(".l");
+
+let equal = document.querySelector(".equal");
+
+let output = document.querySelector(".output");
